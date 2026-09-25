c6d0fb5fd479dc18e704a84f20657482f219aec3
shinigami-gif <shinigami150801@gmail.com>
2026-09-26 04:31:01 +0700

    Add Ktor backend module

---
+++ backend/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.serialization)
+    application
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("shinigami.backend.ApplicationKt")
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.call.logging)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
