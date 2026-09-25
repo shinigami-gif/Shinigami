8e97b8b1ce422b2ada4cbe39814f7c71ba08e8a1
shinigami-gif <shinigami150801@gmail.com>
2026-09-25 20:07:14 +0700

    Support nested list pagers in fragments

--- app/src/main/java/ani/dantotsu/media/user/ListViewPagerAdapter.kt
+++ app/src/main/java/ani/dantotsu/media/user/ListViewPagerAdapter.kt
@@ -1,15 +1,16 @@
 package ani.dantotsu.media.user
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ListViewPagerAdapter(
     private val size: Int,
     private val calendar: Boolean,
-    fragment: FragmentActivity
-) :
-    FragmentStateAdapter(fragment) {
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int = size
     override fun createFragment(position: Int): Fragment =
         ListFragment.newInstance(position, calendar)
