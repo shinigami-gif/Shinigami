1cf97004f04bfc5f27853338c22bab8ca091ece8
shinigami-gif <shinigami150801@gmail.com>
2026-09-25 23:58:35 +0700

    Remove obsolete manga novel torrent interop bindings

--- app/src/main/java/ani/dantotsu/di/injekt/MetroInteropModule.kt
+++ app/src/main/java/ani/dantotsu/di/injekt/MetroInteropModule.kt
@@ -5,24 +5,18 @@ import android.content.Context
 import androidx.media3.database.StandaloneDatabaseProvider
 import ani.dantotsu.addons.download.DownloadAddonManager
 import ani.dantotsu.download.DownloadsManager
-import ani.dantotsu.media.manga.MangaCache
-import ani.dantotsu.parsers.novel.NovelExtensionManager
-import ani.dantotsu.torrent.TorrentServerManager
 import dev.zacsweers.metro.Inject
 import eu.kanade.domain.base.BasePreferences
 import eu.kanade.domain.source.service.SourcePreferences
 import eu.kanade.tachiyomi.extension.anime.AnimeExtensionManager
-import eu.kanade.tachiyomi.extension.manga.MangaExtensionManager
 import eu.kanade.tachiyomi.network.JavaScriptEngine
 import eu.kanade.tachiyomi.network.NetworkHelper
 import eu.kanade.tachiyomi.source.anime.AndroidAnimeSourceManager
-import eu.kanade.tachiyomi.source.manga.AndroidMangaSourceManager
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.protobuf.ProtoBuf
 import tachiyomi.core.preference.PreferenceStore
 import tachiyomi.domain.source.anime.service.AnimeSourceManager
-import tachiyomi.domain.source.manga.service.MangaSourceManager
 import uy.kohesive.injekt.api.InjektModule
 import uy.kohesive.injekt.api.InjektRegistrar
 import uy.kohesive.injekt.api.addSingleton
@@ -38,13 +32,8 @@ class MetroInteropModule(
     private val networkHelper: NetworkHelper,
     private val javaScriptEngine: JavaScriptEngine,
     private val animeExtensionManager: AnimeExtensionManager,
-    private val mangaExtensionManager: MangaExtensionManager,
-    private val novelExtensionManager: NovelExtensionManager,
-    private val torrentServerManager: TorrentServerManager,
     private val downloadAddonManager: DownloadAddonManager,
     private val animeSourceManager: AnimeSourceManager,
-    private val mangaSourceManager: MangaSourceManager,
-    private val mangaCache: MangaCache,
     private val databaseProvider: StandaloneDatabaseProvider,
     private val json: Json,
     private val protoBuf: ProtoBuf,
@@ -66,17 +55,12 @@ class MetroInteropModule(
         addSingleton(javaScriptEngine)
 
         addSingleton(animeExtensionManager)
-        addSingleton(mangaExtensionManager)
-        addSingleton(novelExtensionManager)
-        addSingleton(torrentServerManager)
         addSingleton(downloadAddonManager)
 
         addSingleton(animeSourceManager)
-        addSingleton(mangaSourceManager)
 
         addSingleton(json)
         addSingleton(protoBuf)
         addSingleton(databaseProvider)
-        addSingleton(mangaCache)
     }
 }
