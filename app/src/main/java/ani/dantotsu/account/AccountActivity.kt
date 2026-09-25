9b060e140b2a6a4648a679aa8866d7fcd54e5d84
shinigami-gif <shinigami150801@gmail.com>
2026-09-25 17:30:41 +0700

    feat(nav): wire profile bottom navigation

--- app/src/main/java/ani/dantotsu/account/AccountActivity.kt
+++ app/src/main/java/ani/dantotsu/account/AccountActivity.kt
@@ -7,6 +7,10 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import ani.dantotsu.R
+import ani.dantotsu.MainActivity
+import ani.dantotsu.media.CalendarActivity
+import ani.dantotsu.media.user.ListActivity
+import ani.dantotsu.profile.activity.FeedActivity
 import ani.dantotsu.databinding.ActivityAccountBinding
 import ani.dantotsu.settings.SettingsAboutActivity
 import ani.dantotsu.settings.SettingsActivity
@@ -24,6 +28,46 @@ class AccountActivity : AppCompatActivity() {
 
         binding = ActivityAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.includedNavbar.navbar.selectTabAt(4)
+        binding.includedNavbar.navbar.setOnTabSelectListener(
+            object : nl.joery.animatedbottombar.AnimatedBottomBar.OnTabSelectListener {
+                override fun onTabSelected(
+                    lastIndex: Int,
+                    lastTab: nl.joery.animatedbottombar.AnimatedBottomBar.Tab?,
+                    newIndex: Int,
+                    newTab: nl.joery.animatedbottombar.AnimatedBottomBar.Tab
+                ) {
+                    when (newIndex) {
+                        0 -> {
+                            startActivity(Intent(this@AccountActivity, MainActivity::class.java).putExtra("goToHome", true))
+                            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+                            finish()
+                        }
+                        1 -> {
+                            startActivity(Intent(this@AccountActivity, CalendarActivity::class.java))
+                            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+                            finish()
+                        }
+                        2 -> {
+                            startActivity(Intent(this@AccountActivity, FeedActivity::class.java))
+                            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+                            finish()
+                        }
+                        3 -> {
+                            startActivity(
+                                Intent(this@AccountActivity, ListActivity::class.java)
+                                    .putExtra("anime", true)
+                                    .putExtra("userId", ani.dantotsu.connections.anilist.Anilist.userid)
+                            )
+                            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+                            finish()
+                        }
+                        4 -> Unit
+                    }
+                }
+            }
+        )
 
         loadProfile()
 
