f9b50a0c042de90e8f30e64de31015d39ce7c1f4
shinigami-gif <shinigami150801@gmail.com>
2026-09-26 19:07:27 +0700

    Remove legacy notification receivers

--- app/src/main/java/ani/dantotsu/notifications/AlarmManagerScheduler.kt
+++ app/src/main/java/ani/dantotsu/notifications/AlarmManagerScheduler.kt
@@ -6,8 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import ani.dantotsu.notifications.TaskScheduler.TaskType
-import ani.dantotsu.notifications.anilist.AnilistNotificationReceiver
-import ani.dantotsu.notifications.comment.CommentNotificationReceiver
 import ani.dantotsu.notifications.subscription.SubscriptionNotificationReceiver
 import ani.dantotsu.settings.saving.PrefManager
 import ani.dantotsu.settings.saving.PrefName
@@ -20,35 +18,17 @@ class AlarmManagerScheduler(private val context: Context) : TaskScheduler {
             return
         }
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        val intent = when {
-            taskType == TaskType.COMMENT_NOTIFICATION && PrefManager.getVal<Int>(PrefName.CommentsEnabled) == 1 ->
-                Intent(context, CommentNotificationReceiver::class.java)
-
-            taskType == TaskType.ANILIST_NOTIFICATION ->
-                Intent(context, AnilistNotificationReceiver::class.java)
-
-            taskType == TaskType.SUBSCRIPTION_NOTIFICATION ->
-                Intent(context, SubscriptionNotificationReceiver::class.java)
-
-            else -> return
-        }
-
+        val intent = Intent(context, SubscriptionNotificationReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
             context,
             taskType.ordinal,
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
-
         val triggerAtMillis = System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(interval)
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                alarmManager.setExactAndAllowWhileIdle(
-                    AlarmManager.RTC_WAKEUP,
-                    triggerAtMillis,
-                    pendingIntent
-                )
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent)
             } else {
                 alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent)
             }
@@ -61,20 +41,7 @@ class AlarmManagerScheduler(private val context: Context) : TaskScheduler {
 
     override fun cancelTask(taskType: TaskType) {
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        val intent = when {
-            taskType == TaskType.COMMENT_NOTIFICATION && PrefManager.getVal<Int>(PrefName.CommentsEnabled) == 1 ->
-                Intent(context, CommentNotificationReceiver::class.java)
-
-            taskType == TaskType.ANILIST_NOTIFICATION ->
-                Intent(context, AnilistNotificationReceiver::class.java)
-
-            taskType == TaskType.SUBSCRIPTION_NOTIFICATION ->
-                Intent(context, SubscriptionNotificationReceiver::class.java)
-
-            else -> return
-        }
-
+        val intent = Intent(context, SubscriptionNotificationReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
             context,
             taskType.ordinal,
