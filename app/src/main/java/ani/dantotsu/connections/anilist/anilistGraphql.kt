3295709d0e061300756da735be88b0277c5f2be5
shinigami-gif <shinigami150801@gmail.com>
2026-09-26 20:18:56 +0700

    Rename anime search GraphQL query

--- app/src/main/java/ani/dantotsu/connections/anilist/anilistGraphql.kt
+++ app/src/main/java/ani/dantotsu/connections/anilist/anilistGraphql.kt
@@ -186,7 +186,7 @@ fun userInformation() = """
     siteUrl
 """.prepare()
 
-fun aniMangaSearch(perPage: Int?) = """
+fun searchAnimeQuery(perPage: Int?) = """
     query (${"$"}page: Int = 1, ${"$"}id: Int, ${"$"}type: MediaType, ${"$"}isAdult: Boolean = false, ${"$"}search: String, ${"$"}format: [MediaFormat], ${"$"}status: MediaStatus, ${"$"}countryOfOrigin: CountryCode, ${"$"}source: MediaSource, ${"$"}season: MediaSeason, ${"$"}seasonYear: Int, ${"$"}year: String, ${"$"}onList: Boolean, ${"$"}yearLesser: FuzzyDateInt, ${"$"}yearGreater: FuzzyDateInt, ${"$"}episodeLesser: Int, ${"$"}episodeGreater: Int, ${"$"}durationLesser: Int, ${"$"}durationGreater: Int, ${"$"}chapterLesser: Int, ${"$"}chapterGreater: Int, ${"$"}volumeLesser: Int, ${"$"}volumeGreater: Int, ${"$"}licensedBy: [String], ${"$"}isLicensed: Boolean, ${"$"}genres: [String], ${"$"}excludedGenres: [String], ${"$"}tags: [String], ${"$"}excludedTags: [String], ${"$"}minimumTagRank: Int, ${"$"}sort: [MediaSort] = [POPULARITY_DESC, SCORE_DESC, START_DATE_DESC]) {
       Page(page: ${"$"}page, perPage: ${perPage ?: 50}) {
         $standardPageInformation
