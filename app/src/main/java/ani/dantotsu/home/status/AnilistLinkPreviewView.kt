9210c9f1bd0f68fe69d8d54c547f2a9487c8d2a7
shinigami-gif <shinigami150801@gmail.com>
2026-09-27 01:25:45 +0700

    Fix anime link preview syntax

--- app/src/main/java/ani/dantotsu/home/status/AnilistLinkPreviewView.kt
+++ app/src/main/java/ani/dantotsu/home/status/AnilistLinkPreviewView.kt
@@ -66,7 +66,8 @@ class AnilistLinkPreviewView @JvmOverloads constructor(
                     ""
                 }
             }
-            val animeData = media.anime            val episodesOrChapters = when {
+            val animeData = media.anime
+            val episodesOrChapters = when {
                 animeData != null && animeData.totalEpisodes != null -> {
                     val eps = animeData.totalEpisodes
                     if (eps == 0) null else "$eps Episodes"
