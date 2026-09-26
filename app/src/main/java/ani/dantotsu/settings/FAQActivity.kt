4e90116dbebe91e5f464cb45c926a85ccac0a5e7
shinigami-gif <shinigami150801@gmail.com>
2026-09-26 21:32:54 +0700

    cleanup: remove obsolete FAQ entry

--- app/src/main/java/ani/dantotsu/settings/FAQActivity.kt
+++ app/src/main/java/ani/dantotsu/settings/FAQActivity.kt
@@ -61,11 +61,6 @@ class FAQActivity : AppCompatActivity() {
                 currContext()?.getString(R.string.answer_6) ?: ""
             ),
             Triple(
-                R.drawable.ic_round_movie_filter_24,
-                currContext()?.getString(R.string.question_7) ?: "",
-                currContext()?.getString(R.string.answer_7) ?: ""
-            ),
-            Triple(
                 R.drawable.ic_round_lock_open_24,
                 currContext()?.getString(R.string.question_9) ?: "",
                 currContext()?.getString(R.string.answer_9) ?: ""
